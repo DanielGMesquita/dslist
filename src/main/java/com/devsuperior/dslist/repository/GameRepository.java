9943a5d8f462fa39eb488ba09d602e0b2fe072f0
Daniel Mesquita <danielmesquitadev@gmail.com>
2023-05-08 22:50:45 -0300

    feat: add controller, service and repository for Game

---
+++ src/main/java/com/devsuperior/dslist/repository/GameRepository.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dslist.repository;
+
+import com.devsuperior.dslist.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+}
---
+++ src/main/java/com/devsuperior/dslist/service/GameService.java
@@ -0,0 +1,20 @@
+package com.devsuperior.dslist.service;
+
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.repository.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    @Autowired private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll() {
+        List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+}
